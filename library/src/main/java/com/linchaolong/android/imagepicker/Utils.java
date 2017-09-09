49cc8cf5f9728c77546286d7927fdbc5ca77014f
linchaolong <linchaolong@healthmall.cn>
2017-09-09 18:09:00 +0800

    Utils#getRealPathFromURI, content uri to path

--- library/src/main/java/com/linchaolong/android/imagepicker/Utils.java
+++ library/src/main/java/com/linchaolong/android/imagepicker/Utils.java
@@ -1,8 +1,10 @@
 package com.linchaolong.android.imagepicker;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import java.io.File;
 
@@ -29,6 +31,28 @@ public class Utils {
 
   public static Uri getContentUri(Context context, Uri fileUri){
     return FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".imagePicker.provider", new File(fileUri.getPath()));
+  }
+
+  /**
+   * content uri to path
+   *
+   * @param context
+   * @param contentUri
+   * @return
+   */
+  public static String getRealPathFromURI(Context context, Uri contentUri) {
+    Cursor cursor = null;
+    try {
+      String[] proj = { MediaStore.Images.Media.DATA };
+      cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+      int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+      cursor.moveToFirst();
+      return cursor.getString(column_index);
+    } finally {
+      if (cursor != null) {
+        cursor.close();
+      }
+    }
   }
 
 }
